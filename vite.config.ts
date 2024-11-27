a58b28e5db1a747f8ff521d1da0d69e94ec787b4
Nikolai Vasilenko <nikolai-vasilenko-developer@yandex.ru>
2024-11-27 18:03:54 +0700

    add compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,18 @@ import vue from '@vitejs/plugin-vue'
 import path from "path";
 import 'jsdom';
 import {visualizer} from "rollup-plugin-visualizer";
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), visualizer({
-    emitFile: true,
-    filename: "stats.html",
-  })],
+  plugins: [
+      vue(),
+      visualizer({
+        emitFile: true,
+        filename: "stats.html",
+      }),
+      viteCompression(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
